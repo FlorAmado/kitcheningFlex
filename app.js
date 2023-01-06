d6806d0358f9f42af5c32c14905b309a9d4aa6b1
florenciaamado <amadoflorencia8@gmail.com>
2023-01-05 23:45:55 -0300

    Agregando formulario y botones

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const port = 3030;
 app.use(express.static(path.join(__dirname,  'public' )))
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname, 'views', 'home.html')))
+app.get('/registrar',(req,res) => res.sendFile(path.join(__dirname, 'views', 'registrar.html')))
+app.get('/ingresar',(req,res) => res.sendFile(path.join(__dirname, 'views', 'ingresar.html')))
+
 
 
 
